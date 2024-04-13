6a8a0a858f41553fdf730ccf2a11a2292f581b0d
Maryam <maryamkamareh1995@gmail.com>
2024-04-13 09:55:46 +0330

    list display list filter list editeble

--- product_module/admin.py
+++ product_module/admin.py
@@ -6,4 +6,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ['title']
     }
+    list_display = ['__str__', 'price', 'rating', 'is_active']
+    list_filter = ['rating', 'is_active']
+    list_editable = ['rating', 'is_active']
 admin.site.register(models.Product, ProductAdmin)
