611196327b3aad06560feef070ed2de74bfaee87
Maryam <maryamkamareh1995@gmail.com>
2024-04-13 09:45:55 +0330

    editable=False => readonly

--- product_module/admin.py
+++ product_module/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ['slug']
+    #readonly_fields = ['slug']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
 admin.site.register(models.Product, ProductAdmin)
