8dce4e2140b07a964be13c18bd45c513fc0f260c
Maryam <maryamkamareh1995@gmail.com>
2024-04-16 16:30:00 +0330

    uploade file , save file with python

--- contact_module/views.py
+++ contact_module/views.py
@@ -10,6 +10,16 @@ class ContactUsView(CreateView):
     template_name = 'contact_module/contact_us_page.html'
     form_class = ContactUsModelForm
     success_url = '/contact-us/'
-    # def form_valid(self, form):
-    #     form.save()
-    #     return super().form_valid(form)
+
+def store_file(file):
+    with open('temp/image.jpg', "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_module/create_profile_page.html')
+    def post(self,request):
+        store_file(request.FILES['image'])
+        return redirect('/contact-us/create-profile/')
+
