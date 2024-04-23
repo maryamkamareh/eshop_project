c6180f068ef123bf28cbd94ab88015bd866a24db
Maryam <maryamkamareh1995@gmail.com>
2024-04-23 11:44:35 +0330

    slider

--- home_module/views.py
+++ home_module/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.views import View
 from django.views.generic.base import TemplateView
-
-# def index_page(request):
-#     return render(request, 'home_module/index_page.html')
-
-# class HomeView(View):
-#     def get(self, request):
-#         contex = {}
-#         return render(request, 'home_module/index_page.html', contex)
-from site_module.models import SiteSetting, FooterLinkBox
+from site_module.models import SiteSetting, FooterLinkBox, Slider
 
 
 class HomeView(TemplateView): # in va 2 taye bala ye kar mikonan
     template_name = 'home_module/index_page.html'
     def get_context_data(self, **kwargs):
         contex = super().get_context_data(**kwargs)
-        contex['data'] = 'this is data in home page'
-        return contex
+        contex['sliders'] = Slider.objects.filter(is_active=True)
 
+        return contex
 
 def site_header_component(request):
     setting : SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
