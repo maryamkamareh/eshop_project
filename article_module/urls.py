2f14c30087231f5445836e0813442d987a989a0e
Maryam <maryamkamareh1995@gmail.com>
2024-04-25 11:40:19 +0330

    details of article

--- article_module/urls.py
+++ article_module/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from article_module.views import ArticlesLIstViews
+from article_module.views import ArticlesLIstViews, ArticleDetailView
 
 urlpatterns = [
     path('', ArticlesLIstViews.as_view(), name='articles_list'),
+    path('cat/<str:category>', ArticlesLIstViews.as_view(), name='articles_by_category_list'),
+    path('<pk>', ArticleDetailView.as_view(), name='articles_detail'),
 ]
--- article_module/views.py
+++ article_module/views.py
@@ -1,15 +1,40 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
-from django.views.generic import ListView
-from article_module.models import Article
+from django.views.generic import ListView, DetailView
+from article_module.models import Article, ArticleCategory
 from jalali_date import datetime2jalali, date2jalali
 
 class ArticlesLIstViews(ListView):
     model = Article
-    paginate_by = 5
+    paginate_by = 1
     template_name = 'article_module/article_page.html'
     def get_context_data(self, *args, **kwargs):
         context = super(ArticlesLIstViews, self).get_context_data(*args, **kwargs)
-        context['date'] = datetime2jalali(self.request.user.date_joined)
         return context
+
+    def get_queryset(self):
+        query = super(ArticlesLIstViews, self).get_queryset()
+        query = query.filter(is_active=True)
+        category_name = self.kwargs.get('category') #digita ya mobile , ...
+
+        if category_name is not None:
+            query = query.filter(selected_categories__url_title__iexact=category_name)
+        return query
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_module/article_detail_page.html'
+    def get_queryset(self):
+        query = super(ArticleDetailView, self).get_queryset()
+        query = query.filter(is_active=True)
+        return query
+
+
+def article_categories_component(request: HttpRequest):
+    article_main_category = ArticleCategory.objects.filter(is_active=True, parent_id=None)
+    context = {
+        'main_categories' : article_main_category
+    }
+    return render(request, 'article_module/components/article_categories_component.html', context)
